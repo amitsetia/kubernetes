c557fdffc37ce55274ba191cd0cb7824f951e17a
Amit Setia <amit.2009setia@gmail.com>
2023-06-15 15:30:35 +0800

    mutliple file change

--- terraform-k8s/main.tf
+++ terraform-k8s/main.tf
@@ -1,3 +1,19 @@
-resource "kubectl_manifest" "ns" {
-  yaml_body = templatefile("${path.module}/ns.yaml", {ns = var.namespace })
+#resource "kubectl_manifest" "ns" {
+#  yaml_body = templatefile("${path.module}/ns-sa.yaml", {
+#  		ns = var.namespace 
+#		IAM_ROLE = var.IAM_ROLE_ARN		
+#	})
+#}
+
+data "kubectl_path_documents" "docs" {
+    pattern = "./*.yaml"
+    vars = {
+        ns = var.namespace
+	IAM_ROLE = var.IAM_ROLE_ARN
+    }
+}
+
+resource "kubectl_manifest" "test" {
+    count     = length(data.kubectl_path_documents.docs.documents)
+    yaml_body = element(data.kubectl_path_documents.docs.documents, count.index)
 }
