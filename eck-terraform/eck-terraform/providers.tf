e4843706a840fb658918023971ffea9354fd30d8
Amit Setia <amit.2009setia@gmail.com>
2023-06-23 13:25:12 +0800

    metricbeat added

---
+++ eck-terraform/eck-terraform/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {  
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
